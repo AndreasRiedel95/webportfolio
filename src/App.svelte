1aac6a30f8f00d9d5451888b552ca034c1cb61be
Andreas Riedel <andreas.riedel95@gmail.com>
2020-09-06 11:48:42 +0200

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let name;
+</script>
+
+<main class="active">
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style type="text/scss">
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		&.active {
+			background: red
+		}
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
